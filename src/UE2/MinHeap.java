d2bd6174d83e476c33450e88161191921deaf91c
Stefan <steve260897@gmail.com>
2019-10-27 23:15:23 +0100

    Updated old implementation of MinHeap

--- src/UE2/MinHeap.java
+++ src/UE2/MinHeap.java
@@ -3,18 +3,23 @@ package UE2;
 import java.util.NoSuchElementException;
 
 public class MinHeap<FuckingAngabe> implements MyPriorityQueue<Long> {
+
+	private static final int MIN_INDEX = 1, EMPTY_INDEX = -1;
+
 	private Long[] content;
 	private int maxSize;
 	private int size;
 
 	public MinHeap(final int capacity) {
+		if (capacity <= 0)
+			throw new IllegalArgumentException("Capacity must be positive: " + capacity);
 		content = new Long[capacity + 1];
 		maxSize = capacity;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return size <= 0;
+		return size() <= 0;
 	}
 
 	@Override
@@ -26,133 +31,121 @@ public class MinHeap<FuckingAngabe> implements MyPriorityQueue<Long> {
 	public void insert(final Long val) throws IllegalArgumentException {
 		if (val == null)
 			throw new IllegalArgumentException("Cannot add a null value");
-		if (size == maxSize) {
-			doubleSize();
-		}
+		if (size() == maxSize) doubleSize();
+
 		content[++size] = val; // index 0 left blank
-		upHeap(size);
+		upHeap(size());
 	}
 
 	private void doubleSize() {
 		final Long[] copy = content;
 		// remember to keep one blank field
-		content = new Long[content.length * 2 - 1];
-		maxSize = maxSize * 2;
-		if (copy.length - 1 >= 0) System.arraycopy(copy, 1, content, 1, copy.length - 1);
+		content = new Long[content.length * 2];
+		maxSize *= 2;
+		System.arraycopy(copy, MIN_INDEX, content, MIN_INDEX, copy.length - 1);
 	}
 
+	private boolean validIndex(final int index) {
+		return MIN_INDEX <= index && index <= size();
+	}
 
 	@Override
 	public Long removeMin() throws NoSuchElementException {
-		if (size < 1 || content[1] == null)
+		if (isEmpty())
 			throw new NoSuchElementException("There is no element to be removed");
 
-		swap(1, size);
-		size--;
-		final Long ret = content[size + 1];
-		content[size + 1] = null;
-		System.out.println(this);
+		swap(1, size());
+		final Long ret = content[size()];
+		content[size--] = null;
 		downHeap(1);
 		return ret;
 	}
 
 	@Override
 	public Long min() throws NoSuchElementException {
-		if (size < 1 || content[1] == null)
+		if (isEmpty())
 			throw new NoSuchElementException("There is no element to be looked at");
-		return content[1];
+		return content[MIN_INDEX];
 	}
 
 	public Long get(final int index) {
-		return index <= size && index >= 0 ? content[index] : null;
+		return validIndex(index) ? content[index] : null;
 	}
 
 	@Override
 	public Object[] toArray() {
 		// excluding the first (null) Element.
-		final Object[] obj = new Object[size];
+		final Long[] obj = new Long[size];
 		System.arraycopy(content, 1, obj, 0, size);
 		return obj;
 	}
 
 	public String toString() {
 		final StringBuilder sb = new StringBuilder();
-
-		sb.append("\n");
-		for (int i = 1; i < content.length && content[i] != null; i++)
-			sb.append("[ Index = ")
-					.append(i).append(" | Value = ")
-					.append(content[i]).append(" | LeftChild = ")
-					.append(leftChild(i)).append(" | RightChild = ")
-					.append(rightChild(i)).append(" | Parent = ")
-					.append(parent(i))
-					.append("]\n");
-		sb.append("{ Size = ").append(size).append(" }\n");
-		// sb.append("Validity = " + validity() + " }\n");
-		return sb.toString();
+		for (int i = 1; validIndex(i); i++) {
+			sb.append(String.format("[i=%d, v=%d, left=%d, right=%d, parent=%d%n",
+					i, content[i], leftChild(i), rightChild(i), parent(i)
+			));
+		}
+		return sb.append(String.format("{ Size = %d }%n", size())).toString();
 	}
 
 	private void upHeap(final int p_index) {
 		int v_index = p_index, v_parent = parent(v_index);
 
-		while (v_parent > 0 && content[v_index].compareTo(content[v_parent]) < 0) {
+		while (v_parent != EMPTY_INDEX && content[v_index].compareTo(content[v_parent]) < 0) {
 			swap(v_index, v_parent);
-			v_index = parent(v_index);
+			v_index = v_parent;
 			v_parent = parent(v_index);
 		}
 	}
 
 	private void downHeap(final int index) {
-		System.out.println(index);
-		if (index < 1 || index >= size || leftChild(index) < 0 || rightChild(index) < 0
-				|| content[leftChild(index)].compareTo(content[index]) > 0
-				&& content[rightChild(index)].compareTo(content[index]) > 0) {
-			return;
-		}
-
-		if (rightChild(index) > 0) {
+		final int right_i = rightChild(index), left_i = leftChild(index);
+		if (canDownHeap(index, left_i, right_i)) {
 			// two children
-			if (content[leftChild(index)].compareTo(content[rightChild(index)]) < 0) {
-				if (content[index].compareTo(content[leftChild(index)]) > 0) {
-					swap(leftChild(index), index);
-					downHeap(leftChild(index));
+			if (right_i != EMPTY_INDEX) {
+				// left child is smaller
+				if (content[left_i].compareTo(content[right_i]) < 0) {
+					swap(left_i, index);
+					downHeap(left_i);
+				} else {
+					swap(right_i, index);
+					downHeap(right_i);
 				}
 			} else {
-				if (content[index].compareTo(content[rightChild(index)]) > 0) {
-					swap(rightChild(index), index);
-					downHeap(rightChild(index));
-				}
-			}
-		} else {
-			// just the left child
-			if (content[index].compareTo(content[leftChild(index)]) > 0) {
-				swap(leftChild(index), index);
+				// just the left child
+				swap(left_i, index);
 			}
 		}
+	}
+
+	private boolean canDownHeap(final int index, final int left, final int right) {
+		return validIndex(index) && validIndex(left) &&
+				       (content[left] < content[index] || validIndex(right) && content[right] < content[index]);
+	}
+
+	private Long min(final Long a, final Long b) {
+		return a < b ? a : b;
 	}
 
 	private int parent(final int index) {
-		if (index <= size && index > 1)
-			return index / 2; // odd numbers do not matter (Integer Division!)
-		return -1;
+		// odd numbers do not matter (Integer Division!)
+		return 1 < index && index <= size() ? index / 2 : EMPTY_INDEX;
 	}
 
 	private int leftChild(final int index) {
-		if (index * 2 <= size && index >= 1)
-			return index * 2;
-		return -1;
+		return 1 <= index && index * 2 <= size() ? index * 2 : EMPTY_INDEX;
 	}
 
 	private int rightChild(final int index) {
-		if (index * 2 + 1 <= size && index >= 1)
-			return index * 2 + 1;
-		return -1;
+		return 1 <= index && index * 2 + 1 <= size() ? index * 2 + 1 : EMPTY_INDEX;
 	}
 
-	private void swap(final int index1, final int index2) {
-		final Long temp = content[index1];
-		content[index1] = content[index2];
-		content[index2] = temp;
+	private void swap(final int from, final int to) {
+		final Long temp = content[from];
+		content[from] = content[to];
+		content[to] = temp;
 	}
 
 }
